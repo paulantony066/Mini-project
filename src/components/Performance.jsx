fc0cfaf23935dc264cb3bbd39b09832ba237a21b
PaulvPayyappily <paul.v.payyappily@gmail.com>
2025-02-26 01:17:29 +0530

    Added new components

---
+++ src/components/Performance.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Button(){
+
+    return(
+
+    <div  class="text-center block w-64 p-3 bg-black border border-gray-200 rounded-lg shadow-sm hover:bg-black dark:bg-black dark:border-gray-700 ">
+        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">4.8</h5>
+        <p class="font-normal text-gray-700 dark:text-gray-400">Average Rating</p>
+    </div>
+
+    )
+}
--- src/pages/manager/Manager.jsx
+++ src/pages/manager/Manager.jsx
@@ -3,22 +3,39 @@ import React from "react";
 import Sidebar from "../../components/Sidebar";
 import Employeedetail from "../../components/Employeedetail";
 import Progressbar from "../../components/Progressbar";
+import Button from "../../components/Button";
+import Performance from "../../components/Performance";
+import Table from "../../components/Table";
 
 
 export default function Manager() {
     return (
-        <div class="w-screen h-screen bg-black text-white flex ">
+        <div class="w-screen h-screen bg-black text-white flex overflow-y-auto">
          <div>
             <Sidebar/>
          </div>
             
-            <div class="w-screen">
+            <div class="w-screen overflow-y-auto">
             <div>
                 <Employeedetail/>
             </div>
             <div>
                 <Progressbar/>
             </div>
+            <br />
+            <div class="flex justify-evenly">
+                <Performance/>
+                <Performance/>
+                <Performance/>
+                <Performance/>
+            </div>
+            <br />
+            <div>
+                <Table/>
+                <br />
+                <Button/>
+            </div>
+
             </div>
     </div>
     )
